460f735451e4e397cc04d78757d18a3431be80c0
Douglas Hutchings <11douglash@berkeley.edu>
2016-03-18 17:32:45 +0000

    A script to identify the handle for the waveforms window.

---
+++ identify_windows.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#construct an array of window id's
+WINDOW_STRING="$(xdotool search --onlyvisible .)"
+array=($WINDOW_STRING)
+
+waveforms_window=""
+for element in "${array[@]}"
+do
+	#iterate over the elements
+	#echo out the window
+	window_name=$(xdotool getwindowname $element)
+	if [[ $window_name == *"WaveForms"* ]]
+	then
+		echo "Found waveform"
+		waveforms_window=$element
+	fi
+done
+
+echo "Waveforms Window is " $element
